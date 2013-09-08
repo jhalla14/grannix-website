aa3b27a7ba913ba5c2e093f4ab8f1b19c8436f9d
Joshua Hall <joshua.a.hall.14@dartmouth.edu>
2013-09-08 18:21:34 -0500

    added about us page

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,12 +4,17 @@ import play.*;
 import play.mvc.*;
 
 import views.html.*;
+import views.html.about;
 import views.html.index;
 
 public class Application extends Controller {
   
     public static Result index() {
         return ok(index.render());
+    }
+
+    public static Result about(){
+        return ok(about.render());
     }
   
 }
